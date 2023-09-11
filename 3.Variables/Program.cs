364882e7a9e2b13434311c2375abc45387e37ad5
KhathutsheloCarter <121690232+KhathutsheloCarter@users.noreply.github.com>
2023-09-11 15:10:34 +0200

    Add files via upload

---
+++ 3.Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int age;
+
+            name = "Khathutshelo";
+            age = 23;
+
+            Console.WriteLine("Hi " + name);
+            Console.WriteLine("You are " + age);
+
+            name = "John";
+            age = 25;
+
+            Console.WriteLine("He never liked his name , at age " + age + " He changed to " + name);
+
+            Console.ReadLine();
+        }
+    }
+}
